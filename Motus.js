ff6c08a2e9071da8f7407571001dbf74e11e425e
Evan <evanaguttesrivelon@gmail.com>
2024-12-18 11:59:24 +0100

    fix de petits truc

--- Motus.js
+++ Motus.js
@@ -1,11 +1,11 @@
-// const win = document.getElementById("win");
-// const tryParagraph = document.getElementById("try");
-// const well = document.getElementById("well");
-// const miss = document.getElementById("miss");
-// const notIn = document.getElementById("not");
-// const wordInput = document.getElementById('word');
+const win = document.getElementById("win");
+const tryParagraph = document.getElementById("try");
+const well = document.getElementById("well");
+const miss = document.getElementById("miss");
+const notIn = document.getElementById("not");
+const wordInput = document.getElementById('word');
 
-export function tryWord(word, base) {
+function tryWord(word, base) {
 	word = word.toLowerCase()
 	base = base.toLowerCase()
 	if (word === base) {
@@ -19,12 +19,17 @@ export function tryWord(word, base) {
 		let arrayBase = base.split("");
 		let arrayWord = word.split("");
 
+		let arrayBaseCopy = arrayBase.slice()
+
 		for (let i = 0; i < arrayWord.length; i++) {
+			console.log(arrayBaseCopy);
 			if (arrayBase[i] === arrayWord[i]) {
 				wellPlaced.push(arrayWord[i]);
+				arrayBaseCopy.splice(arrayBaseCopy.indexOf(arrayWord[i]), 1)
 			}
-			else if (arrayBase.includes(arrayWord[i])) {
+			else if (arrayBaseCopy.includes(arrayWord[i])) {
 				missplaced.push(arrayWord[i]);
+				arrayBaseCopy.splice(arrayBaseCopy.indexOf(arrayWord[i]), 1)
 			}
 			else {
 				notInWord.push(arrayWord[i])
@@ -38,17 +43,22 @@ export function tryWord(word, base) {
 	}
 }
 
-export function printWin()
+function resetRendering()
 {
 	wordInput.value = "";
-	win.innerText = "Vous avez gagné";
 	tryParagraph.innerText = "";
 	well.innerText = "";
 	miss.innerText = "";
 	notIn.innerText = "";
 }
 
-export function printGuess(word, result) {
+function printWin()
+{
+	resetRendering()
+	win.innerText = "Vous avez gagné";
+}
+
+function printGuess(word, result) {
 	win.innerText = ""
 	wordInput.value = "";
 	tryParagraph.innerText = word;
@@ -57,13 +67,14 @@ export function printGuess(word, result) {
 	notIn.innerText = "Pas dans le mot: " + result.notInWord.join(", ");
 }
 
-export function guess() {
+function guess() {
 	let base = "dictionnaire";
 	let word = wordInput.value;
-	if (!word)
+	if (!word || !isNaN(parseInt(word)))
 	{
+		resetRendering()
 		tryParagraph.innerText = "Veuillez entrer un mot"
-		return ("No input")
+		return ("Bad input")
 	}
 	let result = tryWord(word, base);
 	if (result === true)
